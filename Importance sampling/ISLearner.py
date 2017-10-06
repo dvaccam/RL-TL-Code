Returns results in Learners + Fix policy gradient + Fix transition model

Importance sampling/ISLearner.py
@@ -86,6 +86,8 @@ class ISLearner:
                 results[run_idx, size_idx] = np.array([alpha_1_target_opt, alpha_1_target_opt, J1_opt], dtype=np.float64)
                 print("Coverged to:", [alpha_1_target_opt, alpha_2_target_opt], "J:", J1_opt, file=self.out_stream)
 
+        return results
+
 
 
     def optimize_policy_parameters(self, target_size, target_task, source_samples=None, source_tasks=None, source_policies=None,
@@ -164,8 +166,7 @@ class ISLearner:
             iter += 1
             step_size -= (0.01 - 0.001) / max_iters
         if iter > max_iters:
-            self.out_stream.write("Did not converge")
-            self.out_stream.write(grad_norm, iter)
+            print("Did not converge;",grad_norm,iter, file=self.out_stream)
         return alpha1, alpha2
 
 

Importance sampling/PolicyFactoryMC.py
@@ -84,10 +84,10 @@ class PolicyMC:
         a2_mask = np.logical_not(a1_mask)
         vel_norm = self.factory.state_reps[:, 1] / self.factory.max_speed
         mu = (a1_mask * self.alpha1 + a2_mask * self.alpha2) * vel_norm
-        all_grads[:,0] = (-np.exp(-((mu - self.factory.action_bins[1])/self.factory.action_noise)**2)*vel_norm) / (self.factory.action_noise*np.sqrt(2*np.pi)*self.choice_matrix[:,0])
-        all_grads[:,-1] = (np.exp(-((mu - self.factory.action_bins[-2])/self.factory.action_noise) ** 2) * vel_norm) / (self.factory.action_noise * np.sqrt(2 * np.pi) * self.choice_matrix[:,-1])
+        all_grads[:,0] = -(np.exp(-((self.factory.action_bins[1] - mu)/(self.factory.action_noise*np.sqrt(2.)))**2)*vel_norm) / (self.factory.action_noise*np.sqrt(2*np.pi)*self.choice_matrix[:,0])
+        all_grads[:,-1] = (np.exp(-((self.factory.action_bins[-2] - mu)/(self.factory.action_noise*np.sqrt(2.)))**2)*vel_norm) / (self.factory.action_noise*np.sqrt(2*np.pi)*self.choice_matrix[:,-1])
         mu_rep = np.hstack([mu.reshape((-1,1))] * (self.factory.action_reps.shape[0] - 2))
-        all_grads[:,1:-1] = ((-np.exp(-((mu_rep - self.factory.action_bins[2:-1])/self.factory.action_noise)**2) + np.exp(-((mu_rep - self.factory.action_bins[1:-2])/self.factory.action_noise)**2)).T*vel_norm).T/(self.factory.action_noise*np.sqrt(2*np.pi)*self.choice_matrix[:,1:-1])
+        all_grads[:,1:-1] = ((-np.exp(-((self.factory.action_bins[2:-1]- mu_rep)/(self.factory.action_noise*np.sqrt(2.)))**2) + np.exp(-((mu_rep - self.factory.action_bins[1:-2])/(self.factory.action_noise*np.sqrt(2.)))**2)).T*vel_norm).T/(self.factory.action_noise*np.sqrt(2*np.pi)*self.choice_matrix[:,1:-1])
         self.log_gradient_matrix[a1_mask,:,0] = all_grads[a1_mask]
         self.log_gradient_matrix[a2_mask,:,1] = all_grads[a2_mask]
 

Importance sampling/main_discrete.py
@@ -284,10 +284,10 @@ Vs = lstd_v.fit(a, predict=True, weights_d=w3, weights_p=w4, weights_r=w6)
 grad = grad_est.estimate_gradient(a, target_policy, weights=w, Q=Qs, V=Vs)
 print(grad, g)'''
 
-target_sizes = list(range(200, 1000, 200)) + list(range(1000, 10000, 1000)) + list(range(10000, 50000 + 1, 10000))
+target_sizes = list(range(1000, 10000, 1000)) + list(range(10000, 50000 + 1, 10000))
 n_runs = 10
 
-out_stream = open('IS.log', 'w', buffering=1)
+'''out_stream = open('IS.log', 'w', buffering=1)
 learner = ISLearner(gamma, pf, lstd_q, lstd_v, grad_est, seed)
 for i in range(len(source_tasks)):
     print("Task:", power_sources[i], file=out_stream)
@@ -296,12 +296,14 @@ for i in range(len(source_tasks)):
 
 print("All tasks", file=out_stream)
 results = learner.learn(target_task, target_sizes, n_runs, source_tasks, source_policies, n_source_samples, out_stream)
-np.save('learning_app_IS_all', np.array(results))
+np.save('learning_app_IS_all', np.array(results))'''
 
+
+out_stream = open('Learning.log', 'w', buffering=1)
+learner = ISLearner(gamma, pf, lstd_q, lstd_v, grad_est, seed)
 print("No tasks", file=out_stream)
 results = learner.learn(target_task, target_sizes, n_runs, None, None, None, out_stream)
 np.save('learning', np.array(results))
-
 
 
 '''out_stream = open('Batch.log', 'w', buffering=1)

Importance sampling/test.py
@@ -3,36 +3,18 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import numpy as np
+from scipy.special import erf
 epsp = (20*0.3)**2
 epsv = (1.4*.3)**2
-xs = np.linspace(-11, 11, 100)
-ys = np.linspace(-0.8, 0.8, 100)
-as_ = np.linspace(-0.9, 0.9, 10)
+xs = np.linspace(0., 1., 100)
+ys = np.linspace(0., 1., 100)
 xs, ys = np.meshgrid(xs, ys)
-xs = xs.flatten()
-ys = ys.flatten()
-
-
-p1 = np.array([-10])
-p3 = np.array([10])
-p2 = np.array([0])
-v1 = np.array([-0.7])
-v3 = np.array([0.7])
-v2 = np.array([0])
-dp1 = np.abs(xs - p1)
-dp2 = np.abs(xs - p2)
-dp3 = np.abs(xs - p3)
-dv1 = np.abs(ys - v1)
-dv2 = np.abs(ys - v2)
-dv3 = np.abs(ys - v3)
-d = np.stack((np.exp(-dp1**2/epsp - dv1**2/epsv), np.exp(-dp1**2/epsp - dv2**2/epsv), np.exp(-dp1**2/epsp - dv3**2/epsv),
-              np.exp(-dp2 ** 2 / epsp - dv1 ** 2 / epsv), np.exp(-dp2**2/epsp - dv2**2/epsv), np.exp(-dp2**2/epsp - dv3**2/epsv),
-              np.exp(-dp3 ** 2 / epsp - dv1 ** 2 / epsv), np.exp(-dp3 ** 2 / epsp - dv2 ** 2 / epsv),np.exp(-dp3 ** 2 / epsp - dv3 ** 2 / epsv),
-              np.ones(dp1.shape[0], dtype=np.float64))).T
-zs = (d*np.array([60., 65., 70., 50., 0., 80., 40., 65., 90., 0.])).sum(axis=1)
 fig = plt.figure()
+zs = np.abs((erf((0.6 - xs*0.7) / (0.2 * np.sqrt(2.))) - erf((0.4 - xs*0.7) / (0.2 * np.sqrt(2.))))/2. - (erf((0.6 - ys*0.7) / (0.2 * np.sqrt(2.))) - erf((0.4 - ys*0.7) / (0.2 * np.sqrt(2.))))/2.)
 ax = fig.add_subplot(111, projection='3d')
-sur = ax.plot_surface(xs.reshape((100,100)), ys.reshape((100,100)), zs.reshape((100,100)))
+sur = ax.plot_surface(xs, ys, zs)
+zs1 = 2*0.7*np.abs(xs-ys)/(np.sqrt(2*np.pi)*0.2)
+sur = ax.plot_surface(xs, ys, zs1)
 plt.show()
 plt.close()
 