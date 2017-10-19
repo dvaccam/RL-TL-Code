Gradient fixes + Batch fixes

Importance sampling/BatchLearner.py
@@ -118,11 +118,18 @@ class BatchLearner:
                         if source_comps[i] != 0.:
                             samples_idx = np.random.choice(source_samples[i]['fs'].shape[0], p=source_relevance[i]/source_relevance[i].sum(), size=source_sizes[i])
                             for k in selected_samples.keys():
-                                selected_samples[k].append(source_samples[i][k][samples_idx])
+                                selected_samples[k].append(source_samples[i][k][samples_idx]) 
                             phi_source_q_sel.append(phi_source_q[i][samples_idx])
                             phi_ns_source_q_sel.append(phi_ns_source_q[i][samples_idx])
                             phi_source_v_sel.append(phi_source_v[i][samples_idx])
                             phi_ns_source_v_sel.append(phi_ns_source_v[i][samples_idx])
+                    if self.v_estimator is not None and self.q_estimator is not None:
+                        self.q_estimator.source_phi_sa = np.vstack(phi_source_q_sel)
+                        self.q_estimator.source_phi_nsa = np.vstack(phi_ns_source_q_sel)
+                        self.q_estimator.source_rewards = np.hstack(selected_samples['r'])
+                        self.v_estimator.source_phi_s = np.vstack(phi_source_v_sel)
+                        self.v_estimator.source_phi_ns = np.vstack(phi_ns_source_v_sel)
+                        self.v_estimator.source_rewards = np.hstack(selected_samples['r'])
 
                     transfer_samples = {'fs': np.vstack([target_samples['fs']] + selected_samples['fs']),
                                         'a': np.vstack([target_samples['a']] + selected_samples['a']),
@@ -134,16 +141,16 @@ class BatchLearner:
                                         'nsi': np.concatenate([target_samples['nsi']] + selected_samples['nsi']),
                                         'nai': np.concatenate([target_samples['nai']] + selected_samples['nai'])}
                     if self.v_estimator is not None and self.q_estimator is not None:
-                        Qs = self.q_estimator.fit(transfer_samples, predict=True,
-                                                  phi_source=np.vstack(phi_source_q_sel),
-                                                  phi_ns_source=np.vstack(phi_ns_source_q_sel))
-                        Vs = self.v_estimator.fit(transfer_samples, predict=True,
-                                                  phi_source=np.vstack(phi_source_v_sel),
-                                                  phi_ns_source=np.vstack(phi_ns_source_v_sel))
+                        Qs = self.q_estimator.fit(target_samples, predict=True,
+                                                  source_weights=np.ones(source_sizes.sum(), dtype=np.float64))
+                        Vs = self.v_estimator.fit(target_samples, predict=True,
+                                                  source_weights=np.ones(source_sizes.sum(), dtype=np.float64))
                     else:
                         Qs = target_task.env.Q[transfer_samples['fsi'], transfer_samples['ai']]
                         Vs = target_task.env.V[transfer_samples['fsi']]
-                    grad = self.gradient_estimator.estimate_gradient(transfer_samples, pol, Q=Qs, V=Vs)
+                    grad = self.gradient_estimator.estimate_gradient(target_samples,
+                                                                     pol.log_gradient_matrix[transfer_samples['fsi'],transfer_samples['ai']],
+                                                                     Q=Qs, V=Vs)
                 else:
                     if self.v_estimator is not None and self.v_estimator is not None:
                         Qs = self.q_estimator.fit(target_samples, predict=True)
@@ -151,7 +158,9 @@ class BatchLearner:
                     else:
                         Qs = target_task.env.Q[target_samples['fsi'], target_samples['ai']]
                         Vs = target_task.env.V[target_samples['fsi']]
-                    grad = self.gradient_estimator.estimate_gradient(target_samples, pol, Q=Qs, V=Vs)
+                    grad = self.gradient_estimator.estimate_gradient(target_samples,
+                                                                     pol.log_gradient_matrix[target_samples['fsi'],target_samples['ai']],
+                                                                     Q=Qs, V=Vs)
             else:
                 if self.v_estimator is not None and self.v_estimator is not None:
                     Qs = self.q_estimator.fit(target_samples, predict=True)
@@ -159,15 +168,17 @@ class BatchLearner:
                 else:
                     Qs = target_task.env.Q[target_samples['fsi'], target_samples['ai']]
                     Vs = target_task.env.V[target_samples['fsi']]
-                grad = self.gradient_estimator.estimate_gradient(target_samples, pol, Q=Qs, V=Vs)
+                grad = self.gradient_estimator.estimate_gradient(target_samples,
+                                                                 pol.log_gradient_matrix[target_samples['fsi'], target_samples['ai']],
+                                                                 Q=Qs, V=Vs)
             grad *= np.array([(0 < alpha1 < 1) or ((alpha1 < 1 or grad[0] < 0) and (alpha1 > 0 or grad[0] > 0)),
                               (0 < alpha2 < 1) or ((alpha2 < 1 or grad[1] < 0) and (alpha2 > 0 or grad[1] > 0))])
             grad_norm = np.linalg.norm(grad)
+            grad /= np.linalg.norm(grad, ord=np.inf) if grad_norm != 0. else 1.
             iter += 1
             step_size -= (0.01 - 0.001) / max_iters
         if iter > max_iters:
-            self.out_logger.write("Did not converge")
-            self.out_logger.write(grad_norm, iter)
+            print("Did not converge;", grad_norm, iter, file=self.out_stream)
         return alpha1, alpha2
 
 

Importance sampling/GradientEstimator.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 class GradientEstimator:
-    def __init__(self, baseline_type):
+    def __init__(self, gamma, baseline_type):
+        self.gamma = gamma
         self.baseline_type = baseline_type
         self.use_source = False
 
@@ -27,10 +28,10 @@ class GradientEstimator:
             gradient = log_gradient*Q.reshape((-1,1)).copy()
             if source_weights is not None:
                 gradient = gradient*weights.reshape((-1,1))
-            gradient = gradient.mean(axis=0)
+            gradient = gradient.mean(axis=0)/(1. - self.gamma)
         if self.baseline_type == 1:
             gradient = log_gradient*(Q-V).reshape((-1,1))
             if source_weights is not None:
                 gradient = gradient*weights.reshape((-1,1))
-            gradient = gradient.mean(axis=0)
+            gradient = gradient.mean(axis=0)/(1. - self.gamma)
         return gradient

Importance sampling/ISLearner.py
@@ -153,7 +153,7 @@ class ISLearner:
                                                                  Q=Qs, V=Vs, source_weights=weights_zeta)
                 '''g = pol.log_gradient_matrix.copy()
                 g = np.transpose(g, axes=(2, 0, 1)) * (target_task.env.Q * target_task.env.zeta_distr)
-                g = np.transpose(g, axes=(1, 2, 0)).sum(axis=(0, 1))
+                g = np.transpose(g, axes=(1, 2, 0)).sum(axis=(0, 1))/(1. - self.gamma)
                 print(grad, grad_J1, g, alpha1, alpha2)'''
             else:
                 if self.q_estimator is not None and self.v_estimator is not None:

Importance sampling/MinMaxEstimator.py
@@ -309,7 +309,7 @@ class MinMaxWeightsEstimator():
                                                                                                 target_policy.choice_matrix) +\
                                  target_policy.choice_matrix * np.minimum(self.delta_delta[i], np.ones_like(self.delta_delta[i])).reshape((-1, 1))
 
-            self.delta_J[i] = grad_J1 - (self.source_policies[i].log_gradient_matrix * (self.source_tasks[i].env.Q * self.source_tasks[i].env.zeta_distr).reshape(self.source_tasks[i].env.Q.shape + (1,))).sum(axis=(0, 1))
+            self.delta_J[i] = grad_J1 - (self.source_policies[i].log_gradient_matrix * (self.source_tasks[i].env.Q * self.source_tasks[i].env.zeta_distr).reshape(self.source_tasks[i].env.Q.shape + (1,))).sum(axis=(0, 1))/(1. - self.gamma)
 
             self.source_samples[i]['eta_1'] = target_policy.log_gradient_matrix[self.source_samples[i]['fsi'], self.source_samples[i]['ai']] * target_Q[w_idx[i]:w_idx[i+1]].reshape((-1, 1))
 
@@ -325,8 +325,8 @@ class MinMaxWeightsEstimator():
             bias = (self.delta_J * self.ns.reshape((-1, 1))).sum(axis=0) / n
             vari = 0.
             for j in range(self.m):
-                bias += (self.source_samples[j]['eta_j'] - w[w_idx[j]:w_idx[j+1]].reshape((-1,1))*self.source_samples[j]['eta_1']).sum(axis=0)/n
-                vari += (((w[w_idx[j]:w_idx[j+1]].reshape((-1,1))*self.source_samples[j]['eta_1'])**2).sum(axis=0) - ((w[w_idx[j]:w_idx[j+1]].reshape((-1,1))*self.source_samples[j]['eta_1']).sum(axis=0))**2/self.ns[j])/n**2
+                bias += (self.source_samples[j]['eta_j'] - w[w_idx[j]:w_idx[j+1]].reshape((-1,1))*self.source_samples[j]['eta_1']).sum(axis=0)/(n*(1. - self.gamma))
+                vari += (((w[w_idx[j]:w_idx[j+1]].reshape((-1,1))*self.source_samples[j]['eta_1'])**2).sum(axis=0) - ((w[w_idx[j]:w_idx[j+1]].reshape((-1,1))*self.source_samples[j]['eta_1']).sum(axis=0))**2/self.ns[j])/(n*(1. - self.gamma))**2
             bias = (bias**2).sum()
             vari = vari.sum()
             return bias + vari
@@ -334,10 +334,10 @@ class MinMaxWeightsEstimator():
         def grad_g(w):
             bias = (self.delta_J * self.ns.reshape((-1, 1))).sum(axis=0) / n
             for j in range(self.m):
-                bias += (self.source_samples[j]['eta_j'] - w[w_idx[j]:w_idx[j + 1]].reshape((-1, 1)) * self.source_samples[j]['eta_1']).sum(axis=0) / n
+                bias += (self.source_samples[j]['eta_j'] - w[w_idx[j]:w_idx[j + 1]].reshape((-1, 1)) * self.source_samples[j]['eta_1']).sum(axis=0) / (n*(1. - self.gamma))
             grad = np.zeros(w.shape + (2,), dtype=np.float64)
             for j in range(self.m):
-                grad[w_idx[j]:w_idx[j+1]] = 2*self.source_samples[j]['eta_1']*(-bias + self.source_samples[j]['eta_1']*w[w_idx[j]:w_idx[j+1]].reshape((-1,1)) - (self.source_samples[j]['eta_1']*w[w_idx[j]:w_idx[j+1]].reshape((-1,1))).sum()/self.ns[j])/n**2
+                grad[w_idx[j]:w_idx[j+1]] = 2*self.source_samples[j]['eta_1']*(-bias/(1. - self.gamma) + self.source_samples[j]['eta_1']*w[w_idx[j]:w_idx[j+1]].reshape((-1,1))/(1. - self.gamma)**2 - (self.source_samples[j]['eta_1']*w[w_idx[j]:w_idx[j+1]].reshape((-1,1))).sum()/(self.ns[j]*(1. - self.gamma)**2))/n**2
             grad = grad.sum(axis=1)
             return grad
 
@@ -345,7 +345,7 @@ class MinMaxWeightsEstimator():
             hess = np.zeros([w.shape[0]]*2 + [2], dtype=np.float64)
             for j1 in range(self.m):
                 for j2 in range(self.m):
-                    hess[w_idx[j1]:w_idx[j1+1], w_idx[j2]:w_idx[j2+1]] = -self.source_samples[j1]['eta_1'].dot((1. + (j1 == j2)/self.ns[j1])*self.source_samples[j2]['eta_1'].T)
+                    hess[w_idx[j1]:w_idx[j1+1], w_idx[j2]:w_idx[j2+1]] = (-self.source_samples[j1]['eta_1']/(1. - self.gamma)**2).dot((1. + (j1 == j2)/self.ns[j1])*self.source_samples[j2]['eta_1'].T)
             hess = hess*2/n**2
             return hess
 

Importance sampling/main_discrete.py
@@ -143,7 +143,7 @@ target_policy = pf.create_policy(alpha_1_target, alpha_2_target)
 lstd_q = LSTD_Q_Estimator(7, 7, 2, 0.45, True, gamma, 0., min_pos, max_pos, target_task.env.min_speed, target_task.env.max_speed,
                           min_act, max_act)
 lstd_v = LSTD_V_Estimator(6, 6, 0.2, True, gamma, 0., min_pos, max_pos, target_task.env.min_speed, target_task.env.max_speed)
-grad_est = GradientEstimator(baseline_type=1)
+grad_est = GradientEstimator(gamma=gamma, baseline_type=1)
 weights_est = MinMaxWeightsEstimator(gamma)
 
 '''xs = source_tasks[0].env.state_reps[:,0]
@@ -202,7 +202,7 @@ w4 = learner.calculate_density_ratios_transition(a, source_tasks[2], target_task
 w5 = learner.calculate_density_ratios_policy(a, source_tasks[2], target_task, source_policies[2], target_policy)
 g = target_policy.log_gradient_matrix.copy()
 g = np.transpose(g, axes=(2, 0, 1)) * (target_task.env.Q * target_task.env.zeta_distr)
-g = np.transpose(g, axes=(1, 2, 0)).sum(axis=(0, 1))
+g = np.transpose(g, axes=(1, 2, 0)).sum(axis=(0, 1))/(1. - gamma)
 Qs = lstd_q.fit(a, predict=True, weights=w*w4*w5)
 Vs = lstd_v.fit(a, predict=True, weights=w*w4)
 grad = grad_est.estimate_gradient(a, target_policy, weights=w, Q=Qs, V=Vs)
@@ -220,6 +220,18 @@ for i in [2]:
     np.save('learning_app_IS_mm' + str(i+1), np.array(results))
 
 '''print("All tasks", file=out_stream)
+results = learner.learn(target_task, target_sizes, n_runs, source_tasks, source_policies, n_source_samples, out_stream)
+np.save('learning_app_IS_all', np.array(results))'''
+
+
+'''out_stream = open('IS.log', 'w', buffering=1)
+learner = ISLearner(gamma, pf, lstd_q, lstd_v, grad_est, None, seed)
+for i in range(len(source_tasks)):
+    print("Task:", power_sources[i], file=out_stream)
+    results = learner.learn(target_task, target_sizes, n_runs, [source_tasks[i]], [source_policies[i]], [n_source_samples[i]], out_stream)
+    np.save('learning_app_IS' + str(i+1), np.array(results))
+
+print("All tasks", file=out_stream)
 results = learner.learn(target_task, target_sizes, n_runs, source_tasks, source_policies, n_source_samples, out_stream)
 np.save('learning_app_IS_all', np.array(results))'''
 